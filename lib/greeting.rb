0c361a8f60d7e73682b6d23f4b2e772ce1fcb58f
Brian Palmer <bpalmer710@gmail.com>
2021-02-23 20:37:21 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -3,12 +3,3 @@
 def greeting(name)
   puts "Hello #{name}. It's nice to meet you."
 end
-
-<<<<<<< HEAD
-def greeting(name)
-=======
-def greeting
->>>>>>> e49155a24059a276f7ee02bbabf0046106382c27
-  puts "Hello #{name}. It's nice to meet you."
-  
-end
